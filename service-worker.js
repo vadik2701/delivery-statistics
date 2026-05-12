2ac7987c8e924c81c5af99c0f93b2ba8bdbffe58
vadik2701 <fandi4vadim@gmail.com>
2026-05-12 11:53:22 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const cacheName = "delivery-stats-v3";
+const appShell = [
+  "/",
+  "/index.html",
+  "/styles.css?v=4",
+  "/app.js?v=4",
+  "/manifest.webmanifest",
+  "/icon.svg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(appShell)),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)))),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    }),
+  );
+});
